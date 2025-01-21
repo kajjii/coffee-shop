c7981df64c53f25fd964cfba88b8dabe19de4b7c
kajjii <murdiv303@gmail.com>
2025-01-21 21:15:17 +0300

    fix name folder

--- src/components/PaginationBtn/PaginationBtn.jsx
+++ src/components/PaginationBtn/PaginationBtn.jsx
@@ -1,28 +1,36 @@
-import './paginationBtn.scss'
+import "./paginationBtn.scss";
 
 const PaginationBtn = ({ totalPages, setPage, page }) => {
-
-    return (
-        <div className="pagination">
-            <button onClick={() => setPage(p => Math.max(p - 1, 1))} disabled={page === 1}>
-                &lt;
+  return (
+    <div className="pagination">
+      <button
+        onClick={() => setPage(p => Math.max(p - 1, 1))}
+        disabled={page === 1}
+      >
+        &lt;
+      </button>
+      <div className="pagination__numbers">
+        {[...new Array(totalPages)].map((_, i) => {
+          const pageNumber = i + 1;
+          return (
+            <button
+              key={i}
+              onClick={() => setPage(pageNumber)}
+              className={pageNumber === page ? "active" : ""}
+            >
+              {pageNumber}
             </button>
-            <div className="pagination__numbers">
-                {[...new Array(totalPages)].map((_, i) => {
-                    const pageNumber = i + 1
-                    return (
-                    <button 
-                        key={i} 
-                        onClick={() => setPage(pageNumber)}
-                        className={pageNumber === page ? 'active' : ''}
-                        >{pageNumber}</button>)
-                })}
-            </div>
-            <button onClick={() => setPage(p => Math.min(p + 1, totalPages))} disabled={page === totalPages}>
-                &gt;
-            </button>
-        </div>
-    );
+          );
+        })}
+      </div>
+      <button
+        onClick={() => setPage(p => Math.min(p + 1, totalPages))}
+        disabled={page === totalPages}
+      >
+        &gt;
+      </button>
+    </div>
+  );
 };
 
 export default PaginationBtn;
